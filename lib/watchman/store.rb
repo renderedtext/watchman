e4089e4fbad26ad81669f42d555a2745fc614e0b
Igor Šarčević <igor@renderedtext.com>
2016-08-04 16:28:13 +0200

    Send gauge metrics

--- lib/watchman/store.rb
+++ lib/watchman/store.rb
@@ -2,9 +2,9 @@ module Watchman::Store
   module_function
 
   def save(name, value)
-    message = "#{name}|#{value}"
+    message = "#{name}:#{value}|g"
 
-    puts "Sending [#{Watchman.host}:#{Watchman.port}]:#{message}"
+    puts "Sending [#{Watchman.host}:#{Watchman.port}] '#{message}'"
 
     UDPSocket.new.send(message, 0, Watchman.host, Watchman.port)
   end
