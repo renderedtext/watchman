dcc22226b7fe8dd6e30a01353991385b9fccc318
Igor Šarčević <igor@renderedtext.com>
2016-08-11 13:43:59 +0200

    Allow timing values

--- lib/watchman.rb
+++ lib/watchman.rb
@@ -3,13 +3,19 @@ require "benchmark"
 require "statsd"
 
 class Watchman
+  class SubmitTypeError < RuntimeError; end
+
   class << self
     attr_accessor :prefix
     attr_accessor :host
     attr_accessor :port
 
-    def submit(name, value)
-      statsd_client.gauge(metric_name_with_prefix(name), value)
+    def submit(name, value, type = :gauge)
+      case type
+      when :gauge   then statsd_client.gauge(metric_name_with_prefix(name), value)
+      when :timing  then statsd_client.timing(metric_name_with_prefix(name), value)
+      else raise SubmitTypeError.new("Submit type '#{type}' is not recognized")
+      end
     end
 
     def benchmark(name)
@@ -19,7 +25,7 @@ class Watchman
         result = yield
       end
 
-      statsd_client.timing(metric_name_with_prefix(name), (time.real * 1000).floor)
+      submit(name, (time.real * 1000).floor, :timing)
 
       result
     end
